5241d371c47fbfd004574d3ff5530e958f16d77a
Ibrahim Ali <44190803+ibrahimalidev@users.noreply.github.com>
2024-05-31 17:49:38 +0400

    Create troll.bat

---
+++ troll.bat
@@ -0,0 +1,20 @@
+@echo off
+:loop
+msg * Komputeri aciq qoyma
+msg * yaxshi ? 
+msg * axirinci defe deyecem
+msg * Birde olmasin yaxshi ?
+msg * 10 a qeder sayiram
+msg * 10
+msg * 9
+msg * 8
+msg * 7
+msg * 6
+msg * 5
+msg * 4
+msg * 3
+msg * 2
+msg * 1
+msg * ...
+msg * Yox xoshuma gelmedi!
+goto loop
